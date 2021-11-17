6b346d7067a0dc024dabd9336db093fb4723da72
AdamCampeau <adamcampeau3@gmail.com>
2021-11-16 21:03:15 -0500

    6.2.7 Update

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -1,11 +1,20 @@
 var repoConatinerEl = document.querySelector("#repos-conatiner");
 var repoSearchTerm = document.querySelector("#repo-search-term");
 
-fetch("https://api.github.com/users/octocat/repos").then(function(response) {
-  response.json().then(function(data) {
-    console.log(data,user);
-  });
-});
+fetch("https://api.github.com/users/octocat/repos") 
+  .then(function(response) {
+    //request was successful
+    if (response.ok) {
+      response.json().then(function(data) {
+        displayRepos(data, user); 
+        console.log(data,user);
+      });
+    } else {
+      alert('Error; GitHub User Not Found');
+    }
+  })
+  .catch(function(error) {
+    // Notice this '.catch()' getting chained onto the end of the '.then()' method
 console.log("outside");
 
 var userFormEl=document.querySelector("#user-form");
@@ -28,6 +37,27 @@ var fromSubmitHandler=function(event) {
 };
 
 var displayRepos=function(repos,searchTerm) {
+  //check of api returned any repos
+  if (repos.length === 0) {
+    repoContainerEl.textContent = "No repositories found.";
+    return; 
+  }
+
+  //create a status element
+  var statusEl=document.createElement("span");
+  statusEl.classList="flex-row align-center";
+
+  // check if current repo has issues or not
+  if (reposi[i].open_issues_count > 0) {
+    statusEl.innerHTML =
+    "<i class='fas fa-times status-icon icon-danger></i>"
+  } else {
+    statusEl.innterHtml = "<i class='fas fa-check-square status-icon icon-success'></i>";
+  }
+
+  //append to container
+  repoEl.appendChild(statusEl);
+
   // clear old content
   repoContainerEl.textContent = "";
   repoSearchTerm.textContent = searchTerm;
